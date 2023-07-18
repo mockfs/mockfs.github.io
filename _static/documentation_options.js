6b2772fd7fe746efd671778c6c21e8f84f3fb46c
David Aguilar <davvid@gmail.com>
2023-07-18 15:21:57 -0700

    mockfs v1.1.3
    Resolves: https://github.com/mockfs/mockfs.github.io/security/dependabot/6

---
+++ _static/documentation_options.js
@@ -0,0 +1,14 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '1.1.3.dev2+g07f3220.d20230718',
+    LANGUAGE: 'en',
+    COLLAPSE_INDEX: false,
+    BUILDER: 'html',
+    FILE_SUFFIX: '.html',
+    LINK_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+    SHOW_SEARCH_SUMMARY: true,
+    ENABLE_SEARCH_SHORTCUTS: true,
+};
